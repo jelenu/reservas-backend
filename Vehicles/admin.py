bb5b734822bf417abdae5b1449dd459e9f73f9ed
JLN27 <jesusleon2700@gmail.com>
2023-11-07 11:13:38 +0100

    added vehicle models and admin

---
+++ Vehicles/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import VehicleModel, Vehicle, Office
+
+@admin.register(VehicleModel)
+class VehicleModelAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'model', 'year_of_manufacture', 'seats', 'doors', 'transmission')
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('model', 'license_plate', 'available', 'location')
+
+@admin.register(Office)
+class OfficeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
---
+++ Vehicles/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Office(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+
+class VehicleModel(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year_of_manufacture = models.PositiveIntegerField()
+    seats = models.PositiveSmallIntegerField()
+    doors = models.PositiveSmallIntegerField()
+    transmission = models.CharField(max_length=20, choices=(('Manual', 'Manual'), ('Automatic', 'Automatic')))
+    image = models.ImageField(upload_to='car_images/', null=True, blank=True)
+
+
+    def __str__(self):
+        return f'{self.brand} {self.model} ({self.year_of_manufacture})'
+    
+
+
+class Vehicle(models.Model):
+    model = models.ForeignKey(VehicleModel, on_delete=models.CASCADE)
+    license_plate = models.CharField(max_length=10, unique=True)
+    available = models.BooleanField(default=True)
+    location = models.ForeignKey(Office, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.model} - {self.license_plate}'
+    
+
